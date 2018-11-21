cf6d07358b8517dddc03f0c06c61745e9b7e812d
Jonathan Kerr <jonathandrewkerr@gmail.com>
2018-11-21 21:31:36 +0000

    Tidy up

--- solution.py
+++ solution.py
@@ -1,3 +1,5 @@
+import typing
+
 class Solution:
     def __init__(self, array_of_apple_trees, alice_number, bob_number):
         self.apple_trees = array_of_apple_trees
@@ -13,13 +15,13 @@ class Solution:
     def _array_is_too_short(self):
         return len(self.apple_trees) < sum([self.alice_number, self.bob_number])
 
-    def _totals(self):
+    def _totals(self) -> typing.Dict[str, typing.Dict]:
         return {
             "alice": self._total(self.alice_number),
             "bob": self._total(self.bob_number)
         }
 
-    def _total(self, person):
+    def _total(self, person) -> typing.Dict[typing.Tuple, int]:
         start_index = 0
         end_index = person
         output = {}
@@ -28,6 +30,7 @@ class Solution:
             start_index += 1
             end_index += 1
         return output
+
     def _potential_solutions(self):
         potential_totals = []
         for alice_index in self._total(self.alice_number):
@@ -38,5 +41,7 @@ class Solution:
                                             )
         potential_totals.sort(reverse=True)
         return potential_totals
+
     def _intersects(self, first_indices, second_indices):
         return first_indices[0] <= second_indices[0] < first_indices[1] or first_indices[0] < second_indices[1] < first_indices[1]
+
